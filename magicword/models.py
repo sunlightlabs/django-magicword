787dba4971b10aa70cbc5be4af4bb2da608b1a9d
Jeremy Carbaugh <jcarbaugh@gmail.com>
2012-07-05 13:34:57 -0400

    update and clean up model, rename enabled to is_enabled

--- magicword/models.py
+++ magicword/models.py
@@ -1,7 +1,13 @@
 from django.db import models
 
+
 class MagicWord(models.Model):
-    password = models.CharField(max_length=32)
-    enabled = models.BooleanField()
+
+    password = models.CharField(max_length=32, unique=True)
+    is_enabled = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ('password',)
+
     def __unicode__(self):
         return self.password

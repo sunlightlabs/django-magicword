b34f8e5e091602b7db370f2514a4d60b35bdc552
Jeremy Carbaugh <jcarbaugh@gmail.com>
2012-07-05 13:35:58 -0400

    clean up of backend, use optional settings parameters for configuration

--- magicword/backends.py
+++ magicword/backends.py
@@ -1,27 +1,36 @@
+from django.conf import settings
 from django.contrib.auth.models import User, get_hexdigest
+
 from magicword.models import MagicWord
 
-class MagicBackend:
-    
+MAGICAUTH_USERNAME = getattr(settings, 'MAGICAUTH_USERNAME', 'guest')
+MAGICAUTH_FIRST_NAME = getattr(settings, 'MAGICAUTH_FIRST_NAME', 'Guest')
+MAGICAUTH_LAST_NAME = getattr(settings, 'MAGICAUTH_LAST_NAME', 'User')
+
+
+class MagicWordBackend(object):
+
     def authenticate(self, username=None, password=None):
-        username = "guest"
-        try:
-            magic_word = MagicWord.objects.get(password=password)
-            if magic_word.enabled:
-                try:
-                    user = User.objects.get(username=username)
-                except User.DoesNotExist:
-                    user = User(username=username, password=get_hexdigest("sha1", "54L7", username))
-                    user.first_name = "Guest"
-                    user.last_name = "User"
-                    user.is_active = True
-                    user.is_staff = False
-                    user.is_superuser = False
-                    user.save()
-                return user
-        except MagicWord.DoesNotExist:
-            pass
-        return None
+
+        username = MAGICAUTH_USERNAME
+
+        if MagicWord.objects.exists(password=password, is_enabled=True):
+
+            try:
+                user = User.objects.get(username=username)
+            except User.DoesNotExist:
+
+                password = get_hexdigest("sha1", "54L7", username)
+
+                user = User(username=username, password=password)
+                user.first_name = MAGICAUTH_FIRST_NAME
+                user.last_name = MAGICAUTH_LAST_NAME
+                user.is_active = True
+                user.is_staff = False
+                user.is_superuser = False
+                user.save()
+
+            return user
 
     def get_user(self, user_id):
         try:
